d32f56ca9045ad49c18ec2524b706bfd65d171d0
Ankur Singh <singankur28@gmail.com>
2026-01-24 19:52:29 +0530

    data preprocess

--- gpt-dev.py
+++ gpt-dev.py
@@ -25,7 +25,20 @@ vocab_size = len(chars)
 stoi = {ch:i for i,ch in enumerate(chars)}
 itos = {i:ch for i,ch in enumerate(chars)}
 
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: ''.join([itos[i] for i in l])
+encode = lambda s: [stoi[c] for c in s] #encoder takes a string and outputs a list of integers each character is mapped to
+decode = lambda l: ''.join([itos[i] for i in l]) #decoder does the opposite of encoder
 
-print(decode([46, 47, 47, 2]))
+#train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+#data loading
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x,y
