7c0bddef4cda16496f60b46ba234257a135d6ca2
Ankur Singh <singankur28@gmail.com>
2026-01-25 12:14:41 +0530

    Fix to 'device errors', indentation errors and add train model

--- gpt-dev.py
+++ gpt-dev.py
@@ -41,6 +41,7 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x,y = x.to(device), y.to(device)
     return x,y
 
 @torch.no_grad()
@@ -93,3 +94,30 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
+    
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+#pytorch optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate)
+
+for iter in range(max_iters):
+
+    #every once in a while evaluate loss on training and val sets
+
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    #sample a batch of data
+    xb, yb = get_batch('train')
+
+    #evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+#generate from the model
+context = torch.zeros((1,1), dtype = torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
